b649038fc93ee463d81341d8a827fd97ae98e793
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-01-04 20:57:27 +0100

    Added XML docs, required XML docs, create XML doc file

--- NLog.ManualFlush/ManualFlushWrapper.cs
+++ NLog.ManualFlush/ManualFlushWrapper.cs
@@ -5,16 +5,27 @@ using NLog.Targets.Wrappers;
 
 namespace NLog.ManualFlush
 {
+    /// <summary>
+    /// Wrapper target for manual controlling of flush behavior.
+    /// </summary>
 	[Target("ManualFlush")]
 	public class ManualFlushWrapper : WrapperTargetBase
 	{
 		private readonly IList<AsyncLogEventInfo> logs = new List<AsyncLogEventInfo>();
 
+        /// <summary>
+        /// Write the event.
+        /// </summary>
+        /// <param name="logEvent">Event to be written.</param>
 		protected override void Write(AsyncLogEventInfo logEvent)
 		{
 			logs.Add(logEvent);
 		}
 
+        /// <summary>
+        /// Flush and clear events.
+        /// </summary>
+        /// <param name="asyncContinuation"></param>
 		protected override void FlushAsync(AsyncContinuation asyncContinuation)
 		{
 			foreach (var log in logs)
@@ -26,6 +37,9 @@ namespace NLog.ManualFlush
 			base.FlushAsync(asyncContinuation);
 		}
 
+        /// <summary>
+        /// Clear the current events.
+        /// </summary>
 		public void EmptyLogs()
 		{
 			logs.Clear();
